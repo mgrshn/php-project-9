f261fa1fc2b6185abe8855cb6d0db3f8dcabb1b0
Максим Гришин <mgr@Maksims-MacBook-Pro.local>
2023-09-14 19:24:46 +0300

    init commit

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This is index.php file with routing.
+ * php version 8.2.10
+ *
+ * @category File
+ * @package  MyPackage
+ * @author   Maxim Grisin <grishinma2009@yandex.ru>
+ * @license  https://opensource.org/license/mit/ The MIT License
+ * @link     not now
+ */
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+use Slim\Middleware\MethodOverrideMiddleware;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+//use Slim\Factory\AppFactory;
+
+$app = AppFactory::create();
+
+$app->get(
+    '/', function (
+        Request $request, Response $response, $args
+    ) {
+        $response->getBody()->write("Here is starting page");
+        return $response;
+    }
+);
+
+$app->run();
